3d56feae873bbb8e2adf145d17a403e82bfb48b7
Eetu <99178022+E3TU@users.noreply.github.com>
2026-06-07 20:34:09 +0300

    Search bar

---
+++ src/components/Search.svelte
@@ -0,0 +1,18 @@
+<input placeholder="Search The Web" class="search" type="text" />
+
+<style>
+	.search {
+		background-color: var(--gray);
+		border: 2px solid var(--purple);
+		padding: 1rem;
+		border-radius: 12px;
+		font-size: 1rem;
+		width: 100%;
+	}
+	.search::placeholder {
+		color: var(--secondary-text);
+	}
+    .search:focus{
+        border: 2px solid var(--primary-text);
+    }
+</style>
