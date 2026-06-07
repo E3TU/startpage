35852604f9786ea8c84c8eabf786d8a4591226da
Eetu <99178022+E3TU@users.noreply.github.com>
2026-06-07 20:33:28 +0300

    Basic clock

---
+++ src/components/Clock.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { derived } from 'svelte/store';
+
+	let date: any = $state(new Date());
+
+	let hour = $derived(date.getHours());
+
+	let min = $derived(date.getMinutes());
+
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			date = new Date();
+		}, 1000);
+	});
+</script>
+
+<div class="clock-container">
+	<p class="clock">{hour}:{min}</p>
+</div>
