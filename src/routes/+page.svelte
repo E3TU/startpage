006a82b1b6271804ff184089e4c0e1212ca9dd9a
Eetu <99178022+E3TU@users.noreply.github.com>
2026-06-07 18:33:57 +0300

    Main container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="container">
+
+</div>
+
+<style>
+    .container{
+        width: 50rem;
+        height: 25rem;
+        background-color: aqua;
+        margin: 0 auto;
+        margin-top: 20rem;
+    }
+</style>
