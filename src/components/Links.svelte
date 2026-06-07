895830f93ad53678ce8afc56b346231481074c86
Eetu <99178022+E3TU@users.noreply.github.com>
2026-06-07 20:42:39 +0300

    Basic layout for links

--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -1,3 +1,36 @@
 <div class="links">
-    
+	<ul>
+		<li class="item">
+            <img>
+			<p>Kick</p>
+		</li>
+        <li class="item">
+            <img>
+			<p>Kick</p>
+		</li>
+        <li class="item">
+            <img>
+			<p>Kick</p>
+		</li>
+        <li class="item">
+            <img>
+			<p>Kick</p>
+		</li>
+	</ul>
 </div>
+
+<style>
+	ul {
+		list-style: none;
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
+	}
+	.item {
+		color: var(--primary-text);
+		height: 6rem;
+		width: 6rem;
+		background-color: var(--gray);
+		border-radius: 12px;
+	}
+</style>
