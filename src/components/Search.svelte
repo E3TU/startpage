4219cfb478ae310810208e6cc8e3f19fbb8e90be
Eetu <99178022+E3TU@users.noreply.github.com>
2026-06-09 00:26:21 +0300

    Update

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -1,6 +1,11 @@
-<input placeholder="Search The Web" class="search" type="text" />
+<div class="search-container">
+	<input placeholder="Search The Web" class="search" type="text" />
+</div>
 
 <style>
+	.search-container{
+		margin-top: 2rem;
+	}
 	.search {
 		background-color: var(--gray);
 		border: 2px solid var(--purple);
@@ -12,7 +17,7 @@
 	.search::placeholder {
 		color: var(--secondary-text);
 	}
-    .search:focus{
-        border: 2px solid var(--primary-text);
-    }
+	.search:focus {
+		border: 2px solid var(--primary-text);
+	}
 </style>
