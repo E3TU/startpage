cb5ea51613dc701317660a293e0e96c969808976
Eetu <99178022+E3TU@users.noreply.github.com>
2026-06-09 00:25:59 +0300

    Update to clock format

--- src/components/Clock.svelte
+++ src/components/Clock.svelte
@@ -5,9 +5,10 @@
 	let date: any = $state(new Date());
 
 	let hour = $derived(date.getHours());
-
 	let min = $derived(date.getMinutes());
 
+	let hourFormatted = $derived(String(hour).padStart(2, '0'));
+	let minFormatted = $derived(String(min).padStart(2, '0'));
 
 	onMount(() => {
 		const interval = setInterval(() => {
@@ -17,5 +18,22 @@
 </script>
 
 <div class="clock-container">
-	<p class="clock">{hour}:{min}</p>
+	<p class="clock">{hourFormatted}:{minFormatted}</p>
 </div>
+
+<style>
+	.clock-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: var(--gray);
+		height: 100%;
+		padding: 0rem 4rem 0rem 4rem;
+		border-radius: 12px;
+		box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+	}
+	.clock {
+		color: var(--primary-text);
+		font-size: 5rem;
+	}
+</style>
