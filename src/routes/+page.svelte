c7f33853282863f619150db59a253b0dc94fa66b
Eetu <99178022+E3TU@users.noreply.github.com>
2026-06-08 22:57:09 +0300

    Flexbox layouturioguaiouguiaerhg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,38 @@
-<div class="container">
+<script>
+	import Search from '../components/Search.svelte';
+	import Clock from '../components/Clock.svelte';
+	import Links from '../components/Links.svelte';
+	import Weather from '../components/Weather.svelte';
+</script>
 
+<div class="container">
+	<div class="small-item">
+		<Clock />
+	</div>
+	<div class="small-item">
+		<Weather />
+	</div>
+	<div class="full-item">
+		<Links />
+		<Search />
+	</div>
 </div>
 
 <style>
-    .container{
-        width: 50rem;
-        height: 25rem;
-        background-color: aqua;
-        margin: 0 auto;
-        margin-top: 20rem;
-    }
+	.container {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		width: 50rem;
+		height: 25rem;
+		/* background-color: aqua; */
+		margin: 0 auto;
+		margin-top: 20rem;
+	}
+	.small-item{
+		flex: 1;
+	}
+	.full-item{
+		flex: 0 0 100%;
+	}
 </style>
