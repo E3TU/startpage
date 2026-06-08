01155463b7e33cf19a24e0e47e60e18dbdde9651
Eetu <99178022+E3TU@users.noreply.github.com>
2026-06-09 00:26:39 +0300

    placeholder for weather component

---
+++ src/components/Weather.svelte
@@ -0,0 +1,15 @@
+<div class="weather">
+	<p>Icon</p>
+	<p>Weather</p>
+	<p>Location</p>
+	<p>Temperature</p>
+</div>
+
+<style>
+	.weather {
+		background-color: var(--gray);
+		height: 100%;
+		border-radius: 12px;
+		box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+	}
+</style>
