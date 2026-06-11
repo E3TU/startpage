554556ec8896262aaa3d756a3eeed98dcd2ff61c
Eetu <99178022+E3TU@users.noreply.github.com>
2026-06-11 21:31:32 +0300

    Get day and month + updated box shadow

--- src/components/Clock.svelte
+++ src/components/Clock.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import { onDestroy, onMount } from 'svelte';
-	import { derived } from 'svelte/store';
 
 	let date: Date = $state(new Date());
 
 	let hour = $derived(date.getHours());
 	let min = $derived(date.getMinutes());
+
+	let month = $derived(date.toLocaleString('default', { month: 'short'}));
+
+	let day = $derived(date.getDate());
 
 	let hourFormatted = $derived(String(hour).padStart(2, '0'));
 	let minFormatted = $derived(String(min).padStart(2, '0'));
@@ -25,21 +28,29 @@
 
 <div class="clock-container">
 	<p class="clock">{hourFormatted}:{minFormatted}</p>
+	<p class="datemonth">{month} {day}</p>
 </div>
 
 <style>
 	.clock-container {
 		display: flex;
 		justify-content: center;
+		flex-direction: column;
 		align-items: center;
 		background-color: var(--gray);
 		height: 100%;
-		padding: 0rem 4rem 0rem 4rem;
+		width: 18rem;
 		border-radius: 12px;
-		box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+		box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 2px 0px, rgba(0, 0, 0, 0.15) 0px 2px 6px 2px;
 	}
 	.clock {
 		color: var(--primary-text);
+		text-shadow: 8px 8px #000;
 		font-size: 5rem;
+	}
+	.datemonth{
+		color: var(--primary-text);
+		font-size: 1.5rem;
+		font-weight: 600;
 	}
 </style>
