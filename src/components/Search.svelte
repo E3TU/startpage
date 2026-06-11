4ad77a8e725e7b0e4d7b0fed7ba32a86425f8f78
Eetu <99178022+E3TU@users.noreply.github.com>
2026-06-11 22:32:13 +0300

    Search using brave

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -1,5 +1,29 @@
+<script lang="ts">
+	let url: string = 'https://search.brave.com/search?q=';
+	let searchTerm = $state('');
+
+	let search = $derived(url + encodeURIComponent(searchTerm));
+
+	$effect(() => {
+		console.log(searchTerm);
+	});
+
+	function handleSubmit(e: Event) {
+		e.preventDefault();
+		window.location.href = search;
+		searchTerm = ("");
+	}
+</script>
+
 <div class="search-container">
-	<input placeholder="Search The Web" class="search" type="text" />
+	<form onsubmit={handleSubmit}>
+		<input
+			bind:value={searchTerm}
+			placeholder="Search The Web"
+			class="search"
+			type="text"
+		/>
+	</form>
 </div>
 
 <style>
@@ -8,18 +32,24 @@
 	}
 	.search {
 		background-color: var(--gray);
-		border: 2px solid var(--purple);
+		border: 3px solid var(--purple);
 		padding: 1rem;
+		color: var(--primary-text);
 		border-radius: 12px;
 		font-size: 1rem;
 		width: 100%;
-				box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 2px 0px, rgba(0, 0, 0, 0.15) 0px 2px 6px 2px;
-
+		box-shadow:
+			rgba(0, 0, 0, 0.3) 0px 1px 2px 0px,
+			rgba(0, 0, 0, 0.15) 0px 2px 6px 2px;
+		outline: none;
+		transition: 0.5s ease-in;
 	}
 	.search::placeholder {
 		color: var(--secondary-text);
+		font-size: 1rem;
 	}
 	.search:focus {
-		border: 2px solid var(--primary-text);
+		transition: 0.5s ease-in;
+		border: 3px solid var(--primary-text);
 	}
 </style>
