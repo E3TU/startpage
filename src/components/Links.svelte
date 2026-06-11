b340c4b5980c5a2f35f3ee6eb639ccb4087a106d
Eetu <99178022+E3TU@users.noreply.github.com>
2026-06-11 21:58:37 +0300

    Favicons + updated design

--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -1,24 +1,24 @@
 <div class="links">
 	<ul>
 		<li class="item">
-			<img />
-			<p>Kick</p>
+			<img src="https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://www.youtube.com&size=64" alt="favicon" width="64px" height="64px" />
+			<p>Test</p>
 		</li>
 		<li class="item">
-			<img />
-			<p>Kick</p>
+			<img src="https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://www.kick.com&size=64" alt="favicon" width="64px" height="64px" />
+			<p>Test</p>
 		</li>
 		<li class="item">
-			<img />
-			<p>Kick</p>
+			<img src="https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://www.tradingview.com&size=64" alt="favicon" width="64px" height="64px" />
+			<p>Test</p>
 		</li>
 		<li class="item">
-			<img />
-			<p>Kick</p>
+			<img src="https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://www.reddit.com&size=64" alt="favicon" width="64px" height="64px" />
+			<p>Test</p>
 		</li>
 		<li class="item">
-			<img />
-			<p>Kick</p>
+			<img src="https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://www.github.com&size=64" alt="favicon" width="64px" height="64px" />
+			<p>Test</p>
 		</li>
 	</ul>
 </div>
@@ -32,12 +32,19 @@
 		justify-content: space-between;
 	}
 	.item {
+		display: flex;
+		justify-content: flex-end;
+		align-items: center;
+		flex-direction: column;
+		gap: 0.5rem;
+		padding-bottom: 1rem;
 		color: var(--primary-text);
 		height: 8rem;
 		width: 8rem;
 		background-color: var(--gray);
 		border-radius: 12px;
-				box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 2px 0px, rgba(0, 0, 0, 0.15) 0px 2px 6px 2px;
-
+		box-shadow:
+			rgba(0, 0, 0, 0.3) 0px 1px 2px 0px,
+			rgba(0, 0, 0, 0.15) 0px 2px 6px 2px;
 	}
 </style>
