34bfa7c97653a1bc696d9821dae19c5f922ebaa1
Eetu <99178022+E3TU@users.noreply.github.com>
2026-06-14 17:52:18 +0300

    Main container and settings component/sidebar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,82 @@
-<script>
+<script lang="ts">
 	import Search from '../components/Search.svelte';
 	import Clock from '../components/Clock.svelte';
 	import Links from '../components/Links.svelte';
 	import Weather from '../components/Weather.svelte';
+	import Settings from '../components/Settings.svelte';
+
+	import Icon from '@iconify/svelte';
+
+	import { toggleMenu, menu } from '$lib/state/menu.svelte';
 </script>
 
-<div class="container">
-	<div class="smallest-item">
-		<Clock />
+<div class="main-container">
+	<div class="widget-container">
+		<div class="smallest-item">
+			<Clock />
+		</div>
+		<div class="small-item">
+			<Weather />
+		</div>
+		<div class="item">
+			<Links />
+			<Search />
+		</div>
 	</div>
-	<div class="small-item">
-		<Weather />
-	</div>
-	<div class="item">
-		<Links />
-		<Search />
-	</div>
+	<button class="open-menu" onclick={toggleMenu}>
+		<Icon class="settings-icon" icon="material-symbols:settings"></Icon>
+	</button>
+	{#if menu.open}
+		<Settings />
+	{/if}
 </div>
 
 <style>
-	.container {
+	.main-container {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		width: 100%;
+		height: auto;
+	}
+	.widget-container {
 		display: flex;
 		flex-direction: row;
 		flex-wrap: wrap;
 		width: 50rem;
-		height: 25rem;
-		/* background-color: aqua; */
-		margin: 0 auto;
+		height: auto;
+		overflow: hidden;
 		margin-top: 20rem;
 		gap: 3rem;
 	}
 	.smallest-item {
 		flex: 1;
-		height: 12rem;
+		height: auto;
 	}
 	.small-item {
 		flex: 2;
-		height: 12rem;
+		height: auto;
 	}
 	.item {
 		flex: 0 0 100%;
+		height: auto;
+	}
+	:global(.settings-icon) {
+		color: var(--primary-text);
+		position: absolute;
+		right: 2rem;
+		top: 2rem;
+		font-size: 2rem;
+		cursor: pointer;
+		transition: ease-in 0.5s;
+	}
+	:global(.settings-icon):hover {
+		color: var(--green);
+		transition: ease-in 0.5s;
+	}
+	.open-menu {
+		background-color: transparent;
+		outline: none;
+		border: none;
 	}
 </style>
