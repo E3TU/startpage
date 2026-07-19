fcc94ea9c9935e7328af0d7dc348e1db799a0bfa
Eetu <99178022+E3TU@users.noreply.github.com>
2026-07-20 00:52:13 +0300

    Added button and prevent empty searches

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Icon from '@iconify/svelte';
+
 	let url: string = 'https://search.brave.com/search?q=';
 	let searchTerm = $state('');
 
@@ -10,25 +12,32 @@
 
 	function handleSubmit(e: Event) {
 		e.preventDefault();
-		window.location.href = search;
-		searchTerm = ("");
+		if(searchTerm) {
+			window.location.href = search;
+		}
+		searchTerm = '';
 	}
 </script>
 
 <div class="search-container">
-	<form onsubmit={handleSubmit}>
-		<input
-			bind:value={searchTerm}
-			placeholder="Search The Web"
-			class="search"
-			type="text"
-		/>
+	<form class="search-form" onsubmit={handleSubmit}>
+		<input bind:value={searchTerm} placeholder="Search The Web" class="search" type="text" />
+		<button id="search-btn"><Icon icon="material-symbols:search-rounded"></Icon></button>
 	</form>
 </div>
 
 <style>
 	.search-container {
+		display: flex;
+		flex-direction: row;
 		padding-top: 2rem;
+		width: 100%;
+	}
+	.search-form {
+		display: flex;
+		flex-direction: row;
+		width: 100%;
+		gap: 1rem;
 	}
 	.search {
 		background-color: var(--gray);
@@ -37,12 +46,14 @@
 		color: var(--primary-text);
 		border-radius: 30px;
 		font-size: 1rem;
-		width: 100%;
 		box-shadow:
 			rgba(0, 0, 0, 0.3) 0px 1px 2px 0px,
 			rgba(0, 0, 0, 0.15) 0px 2px 6px 2px;
 		outline: none;
 		transition: 0.5s ease-in;
+		width: 100%;
+		flex: 1;
+		height: 4rem;
 	}
 	.search::placeholder {
 		color: var(--secondary-text);
@@ -51,5 +62,23 @@
 	.search:focus {
 		transition: 0.5s ease-in;
 		border: 3px solid var(--primary-text);
+	}
+	#search-btn {
+		width: 4rem;
+		height: 4rem;
+		background-color: var(--accent);
+		border: none;
+		border-radius: 30px;
+		font-size: 1.5rem;
+		color: var(--primary-text);
+		flex: 0 0 4rem;
+		cursor: pointer;
+		border: 3px solid transparent;
+		transition: 0.2s ease-in;
+	}
+	#search-btn:hover {
+		border: 3px solid var(--accent);
+		background-color: var(--gray);
+		transition: 0.2s ease-in;
 	}
 </style>
